3dc38c18c9733b0159a4165795c6eadaeeca661a
hsuannn0331 <63030179+hsuannn0331@users.noreply.github.com>
2024-05-31 15:39:15 +0800

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  setInterval(currentTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function currentTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={currentTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
